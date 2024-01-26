4b75a579f71bd896c5062f94a9ba29baa14906e9
kyungmin123 <kimkm3972@naver.com>
2024-01-26 23:13:35 +0900

    change db configuration

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     // 스프링 컨테이너에 직접 넣어주기
     // 상황에 따라 구현 클래스를 변경할 때 사용
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // -> DBMemoryMemberRepository();
+//        return new MemoryMemberRepository(); // -> DBMemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
